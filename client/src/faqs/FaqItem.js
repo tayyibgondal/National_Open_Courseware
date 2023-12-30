433393ef273cbb704f031ed269acef74b50e137f
tayyibgondal <tayyibgondal2003@gmail.com>
2023-12-30 14:50:23 +0500

    Allow admin to answer the unanswered faqs

--- client/src/faqs/FaqItem.js
+++ client/src/faqs/FaqItem.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function FaqItem({ _id, question, answer, createdAt }) {
   const isAdmin = localStorage.getItem("isAdmin");
-  console.log(isAdmin && answer == null)
+  console.log(isAdmin && answer == null);
   return (
     <div className="faq-card">
       <h2 className="faq-question">{question}</h2>
@@ -11,12 +11,16 @@ export default function FaqItem({ _id, question, answer, createdAt }) {
       <p className="faq-info">
         <span className="info-label">Created on:</span>{" "}
         {new Date(createdAt).toLocaleDateString()}
-
       </p>
-      {isAdmin && answer ==null && (
-        <p>
-          Status:&nbsp;<b>Unanswered</b>
-        </p>
+      {isAdmin && answer == null && (
+        <div>
+          <p>
+            Status:&nbsp;<b>Unanswered</b>
+          </p>
+          <Link to={`/faqs/edit/${_id}`} className="details-link">
+           Answer the faq
+          </Link>
+        </div>
       )}
 
       <Link to={`/faqs/${_id}`} className="details-link">
