59b9800277d567db33437deb7a6dd5be738aaace
siddiquifaras <tayyibgondal2003@gmail.com>
2023-12-31 19:19:26 +0500

    Update post card styles

--- client/src/blogs/Post.js
+++ client/src/blogs/Post.js
@@ -1,5 +1,6 @@
 import { formatISO9075 } from 'date-fns';
 import {Link} from "react-router-dom";
+import "./PostCard.css"
 
 export default function Post({ _id, title, summary, cover, content, createdAt, author }) {
   return (
